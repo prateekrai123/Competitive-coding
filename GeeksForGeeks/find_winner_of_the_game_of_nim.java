7aceb843f304165b31dc0a4aba9b8efdb5c1de8f
Prateek <rprateek254@gmail.com>
2020-05-12 16:53:38 +0530

    added files

---
+++ GeeksForGeeks/find_winner_of_the_game_of_nim.java
@@ -0,0 +1,21 @@
+/*package whatever //do not write package name here */
+
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+
+class GFG {
+	public static void main (String[] args) {
+		Scanner sc = new Scanner(System.in);
+		int t = sc.nextInt();
+		while(t-->0){
+		    int n = sc.nextInt();
+		    if(n%2==0){
+		        System.out.println("Player B");
+		    }
+		    else{
+		        System.out.println("Player A");
+		    }
+		}
+	}
+}
